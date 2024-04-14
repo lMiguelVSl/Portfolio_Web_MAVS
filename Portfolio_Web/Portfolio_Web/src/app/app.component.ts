94d28db33f649acf9fa5fb4bad64be2f5b5dc851
Miguel Vargas <miguel.vargas.work@outlook.com>
2024-04-14 10:26:35 -0500

    maintenance section

--- Portfolio_Web/Portfolio_Web/src/app/app.component.ts
+++ Portfolio_Web/Portfolio_Web/src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { shareableComponents } from './components';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, shareableComponents],
+  imports: [RouterOutlet, shareableComponents, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Portfolio_Web';
+
+  hideBody:boolean = true;
 }
