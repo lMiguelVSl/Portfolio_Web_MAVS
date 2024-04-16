0a3ce425fc29f845775700b1de2cdbbe772b769d
Miguel Vargas <miguel.vargas.work@outlook.com>
2024-04-16 18:10:47 -0500

    Projects component enhancement

--- Portfolio_Web/Portfolio_Web/src/app/components/index.ts
+++ Portfolio_Web/Portfolio_Web/src/app/components/index.ts
@@ -1,7 +1,7 @@
 import { MainPageComponent } from "../views/main-page/main-page.component";
-import { BackgroundComponent } from "./background/background.component";
+import { AboutMeComponent } from "./about-me/about-me.component.spec";
 import { ContactComponent } from "./contact/contact.component";
 import { HeaderComponent } from "./header/header.component";
 import { ProjectsComponent } from "./projects/projects.component";
 
-export const sharedComponents = [ContactComponent, ProjectsComponent, BackgroundComponent, HeaderComponent, MainPageComponent];
+export const sharedComponents = [ContactComponent, ProjectsComponent, AboutMeComponent, HeaderComponent, MainPageComponent];
--- Portfolio_Web/Portfolio_Web/src/app/components/projects/projects.component.ts
+++ Portfolio_Web/Portfolio_Web/src/app/components/projects/projects.component.ts
@@ -3,6 +3,27 @@ import { materialComponents } from '../material';
 import { CommonModule } from '@angular/common';
 import { Project } from './models/project-type';
 
+const projectsMock: Project[] = [
+  {
+      Title: "E-commerce Website",
+      Description: "A fully functional e-commerce platform for selling various products online.",
+      Stack: "React, Node.js, MongoDB",
+      ImgUrl: "https://example.com/ecommerce.png"
+  },
+  {
+      Title: "Fitness Tracker App",
+      Description: "An app to track workouts, set fitness goals, and monitor progress over time.",
+      Stack: "Flutter, Firebase",
+      ImgUrl: "https://example.com/fitness_app.png"
+  },
+  {
+      Title: "Sales Performance Dashboard",
+      Description: "A dashboard to analyze sales data, track performance metrics, and visualize trends.",
+      Stack: "Python, Pandas, Matplotlib",
+      ImgUrl: "https://example.com/sales_dashboard.png"
+  }
+];;
+
 @Component({
   selector: 'app-projects',
   standalone: true,
@@ -11,5 +32,5 @@ import { Project } from './models/project-type';
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
-  projects: Project[] = [];
+  projects: Project[] = projectsMock;
 }
