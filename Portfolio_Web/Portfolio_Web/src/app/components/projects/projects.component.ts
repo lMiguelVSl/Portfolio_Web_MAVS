05a1952b9804be3423ae72d18cff11b3c433fff4
Miguel Vargas <miguel.vargas.work@outlook.com>
2024-04-20 13:28:10 -0500

    Projects updates

--- Portfolio_Web/Portfolio_Web/src/app/components/projects/projects.component.ts
+++ Portfolio_Web/Portfolio_Web/src/app/components/projects/projects.component.ts
@@ -8,19 +8,25 @@ const projectsMock: Project[] = [
     Title: "User management",
     Description: "A grid view with CRUD operations to manage user information and operations.",
     Stack: "Angular | .NET | SQL",
-    ImgUrl: ""
+    ImgUrl: "../../../assets/img/user-management-app.png"
   },
   {
     Title: "User Registration",
     Description: "View with registration form and grid view.",
     Stack: "ReactJS | Node.JS | MongoDB",
-    ImgUrl: ""
+    ImgUrl: "../../../assets/img/upet-app.png"
   },
   {
     Title: "Car List",
     Description: "A grid view with different view to manage CRUD operations regarding car registration.",
     Stack: "Angular | .NET | SQL",
-    ImgUrl: ""
+    ImgUrl: "../../../assets/img/cars-app.png"
+  },
+  {
+    Title: "Countries App",
+    Description: "An app to search information about all countries.",
+    Stack: "Angular | Countries API Consuption",
+    ImgUrl: "../../../assets/img/countries-app.png"
   },
   {
     Title: "Gifs",
